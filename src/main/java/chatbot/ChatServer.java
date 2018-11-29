a261ef2b3835caf070ccd0ba852c4528e5986694
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2018-11-29 10:19:08 -0700

    firebase error

--- src/main/java/chatbot/ChatServer.java
+++ src/main/java/chatbot/ChatServer.java
@@ -10,6 +10,7 @@ public class ChatServer extends WebSocketServer {
     public ChatServer() throws UnknownHostException {
     }
 
+
     @Override
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
 
@@ -28,5 +29,15 @@ public class ChatServer extends WebSocketServer {
     @Override
     public void onError(WebSocket webSocket, Exception e) {
 
+    }
+
+    public static void main(String[] args) {
+        int port;
+        try {
+            port = Integer.parseInt(System.getenv("PORT"));
+        } catch (NumberFormatException nfe) {
+            port = 9000;
+        }
+        new ChatServer(port).start();
     }
 }
